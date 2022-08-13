ffe37ee50560720199e00a14e3e1c601c3623e16
varayac <victor.d.araya@gmail.com>
2022-08-13 02:59:07 -0400

    menu js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,15 @@ toggleMenu.addEventListener("click", () => {
   changeMenuIcon();
 });
 
+// Close when menu click menu list
+const navLink = document.querySelectorAll(".nav-link");
+navLink.forEach((navLink) => {
+  navLink.addEventListener("click", () => {
+    menu.classList.add("translate-y-[-200%]");
+    changeMenuIcon();
+  });
+});
+
 function changeMenuIcon() {
   const isContainTraslate = menu.classList.contains("translate-y-[-200%]");
   const icon = isContainTraslate ? "icon-menu" : "icon-close";
